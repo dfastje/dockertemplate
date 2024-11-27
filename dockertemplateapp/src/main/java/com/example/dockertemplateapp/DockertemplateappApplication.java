8731004efd068d8068ea1bd70ed05134bf7da6ab
dfastje <dfastjework@protonmail.com>
2024-11-26 21:39:33 -0500

    adding rest of files from app

---
+++ dockertemplateapp/src/main/java/com/example/dockertemplateapp/DockertemplateappApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockertemplateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertemplateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertemplateappApplication.class, args);
+	}
+
+}
---
+++ dockertemplateapp/src/test/java/com/example/dockertemplateapp/DockertemplateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockertemplateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertemplateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
